bb9a2df03dccdbf4af0e3474ec17b9a24df839cd
sarahysh12 <saraysh22@gmail.com>
2020-04-21 01:59:35 -0700

    set up redux structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import { createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import authReducer from './store/reducers/auth';
+import artworkReducer from './store/reducers/artwork';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    artwork: artworkReducer
+})
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+))
+
+const app = (
+    <Provider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
